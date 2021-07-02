53eca611fbc9261b1b5e708a236b2a065c16a62e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-07-03 00:57:46 +0800

    Add atomic-notedown

---
+++ atomic-notedown/spec/atomic-nodedown-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomicNotedownView from '../lib/atomic-notedown-view';
+
+describe('AtomicNotedownView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
